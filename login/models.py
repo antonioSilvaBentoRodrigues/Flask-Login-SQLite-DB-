dafbb0525c6e04591a20240d6a40de961a48bf55
Antonio <antonio.bento.rodrigues@hotmail.com>
2024-10-02 19:03:07 +0100

    .

--- login/models.py
+++ login/models.py
@@ -4,5 +4,5 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
-    userName = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    userName = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
